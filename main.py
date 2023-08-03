69b788950e4cfb89a5923e3aea9952ba18f82d1b
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-03 18:36:44 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#n1 = int(input('Digite um número: '))
+#s = n1 - 1
+#s2 = n1 + 1
+#print('o antecessor de {} é o número {}, e o sucessor desse número é o {}'.format(n1, s, s2))
+
+#n1 = int(input('Digite um número: '))
+#s1 = n1 * 2
+#s2 = n1 * 3
+#s3 = n1**1/2
+#print('O dobro do número {} é o {}, o triplo é {}, e a raiz quadrada é {}'.format(n1, s1, s2, s3))
+
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+s = (n1 + n2) / 2
+print('A media da primeira nota {}, e da segunda nota {}, é {}'.format(n1, n2, s))
