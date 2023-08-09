2573bd250a6de60a5f1568fca3aea2fd314fd4c4
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-08 21:43:36 -0300

    Add files via upload

--- codes/ex003.py
+++ codes/ex003.py
@@ -6,4 +6,4 @@ import math
 cat_op = float(input('Qual o tamanho do cateto oposto? '))
 cat_adj = float(input('Qual o tamnhgo do cateto adjacente? '))
 hipo = (cat_adj * cat_adj + cat_op * cat_op)
-print('O valor da hipotenusa é {}'.format(math.sqrt(hipo)))
+print('O valor da hipotenusa é {}'.format(math.sqrt(hipo))) 
