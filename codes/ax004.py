1a03b9749ef57d7525df77f96ffa7fc483373f0f
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-11 22:05:13 -0300

    Add files via upload

---
+++ codes/ax004.py
@@ -0,0 +1,12 @@
+frase = 'Só é verdadeiramente livre quem está sempre pronto a morrer.'
+print(len(frase))
+print(frase[0:6])
+print(frase.count('o'))
+print(frase.find('x'))
+print('ç' in frase)
+print(frase.replace('Só','Apenas'))
+fra_slit = (frase.split())
+print(fra_slit[3])
+
+frase2 = '   Para que falar? Resta-me agir.   '
+print(frase2.strip())
