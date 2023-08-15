449e3d8108334b82dd270beecf43641f0f9477d3
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-14 21:27:32 -0300

    Add files via upload

---
+++ codes/ax005.py
@@ -0,0 +1,7 @@
+not1 = float(input('A sua primeira nota foi: ?'))
+not2 = float(input('A sua segunda nota foi: ?'))
+med = (not1 + not2) / 2
+if med >= 6.0:
+    print('Parabéns sua nota foi boa')
+else:
+    print('Melhore')
