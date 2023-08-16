9d85bc5fbd24f0b6bd33d4b780e4a4aaa26f7710
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-15 22:13:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-name = str(input('Digite seu nome completo: ')).strip()
-print('Analisando seu nome...')
-print('Seu nome em maiúslucas é {}'.format(name.upper()))
-print('Seu nome em minúsculas é {}'.format(name.lower()))
-print('Seu nome tem ao todo {} letras'.format(len(name)- name.count(' ')))
-na_spl = (name.split())
-print('Seu primeiro nome é {}, e tem ao todo {} letras'.format(na_spl[0], len(na_spl[0])))
-
-n = int(input('Digite um número de 0 a 9999: '))
-u = n // 1 % 10
-d = n // 10 % 10 
-c = n // 100 % 10
-m = n // 1000 % 10
-print('Unidade: {}'.format(u))
-print('Dezena: {}'.format(d))
-print('Centena: {}'.format(c))
-print('Milhar: {}'.format(m))
-
-city = str(input('Digite a sua cidade: ')).strip()
-print(city[:5].upper() == 'SANTO')
-
-sobr = str(input('Digite seu nome completo: ')).strip()
-print('Seu nome tem Silva? {}'.format('silva' in sobr.lower()))
-
-# fra = str(input('Digite uma frase: '))
-# print('A letra A aparece {} vezes na frase'.format(fra.count('a')))
-# print('A letra A aparece pela primeira vez na posição {} da frase'.format(fra.find('a')))
-# print('A letra A aparece pela última vez na posição {} da frase'.format(fra.rfind('a')))
-
-# sobr_nam = str(input('Digite seu nome completo: '))
-# pri_nam = sobr_nam.split()
-# ult_nam = sobr_nam.split()
-# print('O seu nome completo é {}'.format(sobr_nam))
-# print('O Primeiro nome é {}'.format(pri_nam[0]))
-# print('O segundo nome é {}'.format(ult_nam[9]))
+not1 = float(input('A sua primeira nota foi: ?'))
+not2 = float(input('A sua segunda nota foi: ?'))
+med = (not1 + not2) / 2
+if med >= 6.0:
+    print('Parabéns sua nota foi boa')
+else:
+    print('Melhore')
