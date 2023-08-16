430aa5bd56f9cb1dbb3c834f3cb2e0ff2ccac01e
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-15 22:14:11 -0300

    Add files via upload

--- codes/ex005.py
+++ codes/ex005.py
@@ -12,32 +12,65 @@ if compu == pla:
 else:
     print('Você errou, eu pensei no número {} e não no número {}'.format(compu, pla))
 
-# velo = int(input('A velocidade do carro era? '))
-# if velo > 80:
-#     print('Voce foi multado em R${}'.format(velo))
-# else:
-#     print('Voce está na velocidade permitida')
+velo = float(input('Qual a velocidade atual do carro? '))
+if velo > 80:
+    print('Você exedeu o limite de velocidade permitido')
+    multa = (velo - 80) * 7
+    print('Você foi multado em R${:.2f}'.format(multa))
+print('Dirija com segurança')
 
-# n = int(input('Digite um número para ver se é par ou impar: '))
-# if n / 2 :
-#     print('Ele é par')
-# else:
-#     print('Impar')
+n = int(input('Digite um número para ver se é par ou impar: '))
+res = n % 2
+if res == 0:
+    print('O seu número é par')
+else:
+    print('O seu número é impar')
 
-# dist = float(input('A distância da viagem em KMs é: '))
-# if dist <= 200:
-#     print('O valor da viagem será de R${}'.format(dist * 0.50))
-# else:
-#     print('O valor da viagem será de R${}'.format(dist * 0.45))
+dist = float(input('A distância da viagem em KMs é: '))
+if dist <= 200:
+    pre = dist * 0.50
+else:
+    pre = dist * 0.45
+print('O preço da viagem será de R${:.2f}'.format(pre))
 
-# ano = int(input('Digite os dias do ano: '))
-# if ano <= 365:
-#     print('Esse ano não é bissexto')
-# else:
-#     print('Esse ano é bissexto')
+from datetime import date
+ano = int(input('Digite o ano, se quiser o ano atual coloque zero: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano de {} é bissexto'.format(ano))
+else:
+    print('O ano de {} não é bissexto'.format(ano))
 
-# sal = float(input('Digite seu salário: '))
-# if sal >= 1250:
-#     print('O aumento do salário foi de R${}'.format(sal * 0.1))
-# else:
-#     print('O aumento do salário foi de R${}'.format(sal * 0.15))
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+menor = num2
+if num1<num2 and num1<num3:
+    menor = num1
+if num3<num2 and num3<num1:
+    menor = num3
+maior = num2
+if num1>num2 and num1>num3:
+    maior = num1
+if num3>num2 and num3>num1:
+    maior = num3
+print('O menor número foi {} e o maior {}'.format(menor, maior))
+
+sal = float(input('Digite seu salário:R$ '))
+if sal <= 1250:
+    sal_n = sal + (sal * 15 / 100)
+else:
+    sal_n = sal + (sal * 10 / 100)
+print('O seu salario de R${} aumentou para R${}'.format(sal, sal_n))
+
+print('-=-' * 20)
+print('analisador de triângulos')
+print('-=-' * 20)
+r1 = float(input('Primeiro segmento de reta: '))
+r2 = float(input('Segundo segmento de reta: '))
+r3 = float(input('Terceiro segmento de reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos formam um triangulo')
+else: 
+    print('Os segmentos acima não formam triangulos5')
