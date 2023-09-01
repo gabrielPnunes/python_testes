85fc36933524865bb39208cc6490c53ec3290925
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-31 22:19:36 -0300

    Add files via upload

--- codes/ex008.py
+++ codes/ex008.py
@@ -1,7 +1,23 @@
-from time import sleep
-timer = int(input('Digite a comtagem regressiva: '))
-for time in range(timer, 0, -1):
-    time + 1
-    sleep(1)
-    print(time)
-print('Explosão, explosão ')
+# from time import sleep
+# for time in range(10, -1, -1):
+#     sleep(0.3)
+#     print(time)
+# print('BUM!, BANG!, POOOO!')
+
+# for par in range(2, 51, 2):
+#     print(par, end=' ')
+# print('acabou')
+
+soma = 0
+count = 0
+for qui in range(1, 501, 2):
+    if qui % 3 == 0:
+        count = count + 1
+        soma = soma + qui
+print('A soma dos {} valores enviados é {} '.format(count, soma))
+    
+# n = int(input('Digite um número para formar uma tabuada: '))
+# for tabuada in range(1, 11):
+#     n * 1
+#     n * 2
+#     print(tabuada)
